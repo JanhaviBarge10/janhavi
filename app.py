fef4f051579e35763f58bff3f1d08e5907bde922
Janhavi <janhavibarge10@gmail.com>
2024-04-15 12:39:48 +0530

    pythone file

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+from redis import Redis 
+
+app = Flask(__name__)
+redis = Redis(host='redis', port=6379
+
+@app.rout('/')
+def index():
+    redis.incr('hits')
+    count = redis.get('hits').decode('utf-8')
+    return render_template('index.html', count=count)
+
+if__name__ == '__main__':
+app.run(debug=True, host='0.0.0.0')
+        
